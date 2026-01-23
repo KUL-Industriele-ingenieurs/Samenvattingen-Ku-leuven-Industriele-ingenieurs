93928bbcccec85c0456dbf01281f4a0645e9d424
Eggmansmile <ruben.ryckaert89@gmail.com>
2026-01-23 16:10:48 +0100

    updated settings and fixed bugin latex build. Added cache system for pdf building and sped up local pdf building for testing.

--- .vscode/scripts/check_issues.ps1
+++
@@ -1,4 +0,0 @@
-# Find common issue markers in .tex files and print as compiler-like warnings
-Get-ChildItem -Recurse -Filter '*.tex' -ErrorAction SilentlyContinue |
-  Select-String -Pattern '@issue|@todo|TODO|FIXME|TODO:' |
-  ForEach-Object { Write-Output ($_.Path + ':' + $_.LineNumber + ':1: warning: ' + ($_.Line.Trim())) }
